6862497ee883c497ad99dfce3d05694090ed6913
Ilya Smelkov <triplepointfive@gmail.com>
2014-10-14 22:24:36 +0400

    Completed sign function, moved to stateT

--- Key.hs
+++ Key.hs
@@ -1,18 +1,56 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, DeriveGeneric #-}
 
 module Key where
 
+import           Data.Bits
+import           Data.Binary
+import           Data.Binary.Get
 import qualified Data.ByteString as B
 import           Data.Word
+import           GHC.Generics (Generic)
 
-countCrcMD4 :: [Word32] -> String -> Int -> [Word32]
-countCrcMD4 = undefined
-
-data Key where Key :: Key
-
+data Key where
+  Key :: { arwNKey :: Int } -> Key
 
 newKey :: Key
 newKey = undefined
 
 loadFromBuffer :: Key -> Int -> Bool
-loadFromBuffer
+loadFromBuffer = undefined
+
+keyValid :: Key -> Bool
+keyValid = undefined
+--   if(!keys.wEKeyBase || !keys.wNKeyBase)
+
+countCrcMD4 :: String -> IO String
+countCrcMD4 = undefined
+
+getCLenB :: Int -> Int -> Int
+getCLenB = undefined
+
+crpB :: String -> Key -> IO String
+crpB = undefined
+
+data KeyFileFormat where
+  KeyFileFormat :: { wReserved1 :: Word16
+                   , wSignFlag  :: Word16
+                   , dwCRC0     :: Word32
+                   , dwCRC1     :: Word32
+                   , dwCRC2     :: Word32
+                   , dwCRC3     :: Word32
+                   , dwLenBuf   :: Word32
+                   , ptrBuffer  :: Word8
+                   } -> KeyFileFormat deriving (Show, Generic)
+
+instance Binary KeyFileFormat where
+  get = do
+    reserved <- getWord16host
+    sign <- getWord16host
+    crc0 <- getWord32host
+    crc1 <- getWord32host
+    crc2 <- getWord32host
+    crc3 <- getWord32host
+    lenBuf <- getWord32host
+    buf <- getWord8
+    return $ KeyFileFormat reserved sign crc0 crc1 crc2 crc3 lenBuf buf
+
--- Signer.hs
+++ Signer.hs
@@ -1,11 +1,12 @@
-{-# LANGUAGE GADTs, DeriveGeneric #-}
+{-# LANGUAGE GADTs, CPP #-}
 
 module Signer where
 
-import           Data.Bits
-import qualified Data.Bytestring as B (readFile, length, empty)
-import           Data.Word
-import           GHC.Generics (Generic)
+import           Control.Monad.State
+import qualified Data.ByteString as B (ByteString, readFile, length, empty)
+import           Data.Char (ord)
+import           Data.Functor ((<$>))
+import           System.Random (randomIO)
 
 import           Key
 import           WMSettings
@@ -18,9 +19,10 @@ data Signer where
             --, ignoreIniFile :: Bool
             , kwmFileFromCL :: Bool
             , key64         :: Bool
-            , szKeyData     :: ByteString
+            , keyData       :: B.ByteString
             , m_siErrorCode :: Int
             , keys          :: Key
+            , szSign        :: String
             } -> Signer
 
 newSigner :: WMSetting -> Signer
@@ -31,112 +33,152 @@ newSigner settings =
          , ignoreKeyFile = False
          , kwmFileFromCL = isKWMFileFromCL settings
          , key64         = key64Flag settings
-         , szKeyData     = B.empty
+         , keyData       = szKeyData settings
          , m_siErrorCode = 0
          , keys          = newKey
+         , szSign        = ""
          }
 
-data KeyFileFormat where
-  KeyFileFormat :: { wReserved1 :: Word16
-                   , wSignFlag  :: Word16
-                   , dwCRC0     :: Word32
-                   , dwCRC1     :: Word32
-                   , dwCRC2     :: Word32
-                   , dwCRC3     :: Word32
-                   , dwLenBuf   :: Word32
-                   , ptrBuffer  :: Word8
-                   } -> KeyFileFormat deriving (Show, Generic)
+io :: IO a -> StateT Signer IO a
+io = liftIO
 
-instance Binary KeyFileFormat where
-  get = do
-    reserved <- getWord16host
-    sign <- getWord16host
-    crc0 <- getWord32host
-    crc1 <- getWord32host
-    crc2 <- getWord32host
-    crc3 <- getWord32host
-    lenBuf <- getWord32host
-    buf <- getWord8
-    return $ KeyFileFormat reserved sign crc0 crc1 crc2 crc3 lenBuf buf
+us2sz :: String -> Int -> String
+us2sz = undefined
 
-secureKeyByIDPWHalf :: Signer -> ByteString -> Int -> Bool
-secureKeyByIDPWHalf singer buf dwBuf =
-  if wSignFlag keyData == 0
-    then
-      m_siErrorCode = -2
-      False
-  else
-    szIDPW = szLogin signer
-    let len = ( length $ szPassword signer ) / 2 in -- Recheck
-    if len > 0
-      szIDPW = szIDPW ++ take len ( szPassword signer )
-    dwCRC = countCrcMD4 dwCRC szIDPW (length szIDPW)
-    -- dwCRC SwitchIndian???
-    char *ptrKey = ptrBuffer keyData
-    DWORD dwKeyLen = dwBuf-(ptrKey-buf) - 6;
-    ptrKey += 6;
-    for(DWORD dwProc=0; dwProc<dwKeyLen; dwProc+=sizeof(dwCRC))
-      for(int k=0; k<sizeof(dwCRC)&&(dwProc+k)<dwKeyLen; k++)
-        *(ptrKey+dwProc+k) ^= ((char *)dwCRC)[k];
-    True
-  where
-    keyData = decode buf :: KeyFileFormat
+-- secureKeyByIDPWHalf :: Signer -> ByteString -> Int -> Bool
+-- secureKeyByIDPWHalf singer buf dwBuf =
+--   if wSignFlag keyData == 0
+--     then
+--       m_siErrorCode = -2
+--       False
+--   else
+--     szIDPW = szLogin signer
+--     let len = ( length $ szPassword signer ) / 2 in -- Recheck
+--     if len > 0
+--       szIDPW = szIDPW ++ take len ( szPassword signer )
+--     dwCRC = countCrcMD4 dwCRC szIDPW (length szIDPW)
+--     -- dwCRC SwitchIndian???
+--     char *ptrKey = ptrBuffer keyData
+--     DWORD dwKeyLen = dwBuf-(ptrKey-buf) - 6;
+--     ptrKey += 6;
+--     for(DWORD dwProc=0; dwProc<dwKeyLen; dwProc+=sizeof(dwCRC))
+--       for(int k=0; k<sizeof(dwCRC)&&(dwProc+k)<dwKeyLen; k++)
+--         *(ptrKey+dwProc+k) ^= ((char *)dwCRC)[k];
+--     True
+--   where
+--     keyData = decode buf :: KeyFileFormat
 
-secureKeyByIDPW :: Signer -> ByteString -> Int -> Bool
-secureKeyByIDPW singer buf dwBuf =
-  if wSignFlag keyData == 0
-    then
-      m_siErrorCode = -2
-      False
-  else
-    dwCRC = countCrcMD4 dwCRC (szLogin signer ++ szPassword signer) (length szIDPW)
-    -- dwCRC SwitchIndian???
-    char *ptrKey = ptrBuffer keyData
-    DWORD dwKeyLen = dwBuf-(ptrKey-buf) - 6;
-    ptrKey += 6;
-      for(DWORD dwProc=0; dwProc<dwKeyLen; dwProc+=sizeof(dwCRC))
-        for(int k=0; k<sizeof(dwCRC)&&(dwProc+k)<dwKeyLen; k++)
-          *(ptrKey+dwProc+k) ^= ((char *)dwCRC)[k];
-    True
-  where
-    keyData = decode buf :: KeyFileFormat
+-- secureKeyByIDPW :: Signer -> ByteString -> Int -> Bool
+-- secureKeyByIDPW singer buf dwBuf =
+--   if wSignFlag keyData == 0
+--     then
+--       m_siErrorCode = -2
+--       False
+--   else
+--     dwCRC = countCrcMD4 dwCRC (szLogin signer ++ szPassword signer) (length szIDPW)
+--     -- dwCRC SwitchIndian???
+--     char *ptrKey = ptrBuffer keyData
+--     DWORD dwKeyLen = dwBuf-(ptrKey-buf) - 6;
+--     ptrKey += 6;
+--       for(DWORD dwProc=0; dwProc<dwKeyLen; dwProc+=sizeof(dwCRC))
+--         for(int k=0; k<sizeof(dwCRC)&&(dwProc+k)<dwKeyLen; k++)
+--           *(ptrKey+dwProc+k) ^= ((char *)dwCRC)[k];
+--     True
+--   where
+--     keyData = decode buf :: KeyFileFormat
 
-loadKeys :: Signer -> IO Signer
-loadKeys signer =	do
-  if not ( key64 signer ) && not ( ignoreKeyFile signer )
-    then
-      pBufRead <- B.readFile ( szKeyFileName signer )
-      processKey ( B.length pBufRead == 164 ) pBufRead
-    else
-      processKey True ( szKeyData signer )
-  where
-    bNotOldFmt = false
+loadKeys :: StateT Signer IO Bool
+loadKeys = undefined
+--   if not ( key64 signer ) && not ( ignoreKeyFile signer )
+--     then
+--       pBufRead <- B.readFile ( szKeyFileName signer )
+--       processKey ( B.length pBufRead == 164 ) pBufRead
+--     else
+--       processKey True ( szKeyData signer )
+--   where
+--     bNotOldFmt = false
 
-    processKey bKeysReaded pBufRead = do
-      if bKeysReaded
-        then
-        ------------
-          secureKeyByIDPWHalf signer pBufRead 164
-          errLoadKey = loadFromBuffer (keys signer) (key { wSignFlag = 0 }) 164
-          if errLoadKey
-            then
-              secureKeyByIDPWHalf signer pBufRead 164 -- restore buffer
-              secureKeyByIDPW signer pBufRead 164
-              --signer {keys = keys signer { wSignFlag = 0 } }
-              errLoadKey = loadFromBuffer (keys signer) (key { wSignFlag = 0 }) 164
-            else
-              signer {keys = keys signer { wSignFlag = 0 } }
-          if not errLoadKey
-            True
-          else
-            signer { keys = newKey }
-            m_siErrorCode = -3
-            False
-     else
-        return bKeysReaded
-      where key = decode pBufRead :: KeyFileFormat
+--     processKey bKeysReaded pBufRead = do
+--       if bKeysReaded
+--         then
+--         ------------
+--           secureKeyByIDPWHalf signer pBufRead 164
+--           errLoadKey = loadFromBuffer (keys signer) (key { wSignFlag = 0 }) 164
+--           if errLoadKey
+--             then
+--               secureKeyByIDPWHalf signer pBufRead 164 -- restore buffer
+--               secureKeyByIDPW signer pBufRead 164
+--               --signer {keys = keys signer { wSignFlag = 0 } }
+--               errLoadKey = loadFromBuffer (keys signer) (key { wSignFlag = 0 }) 164
+--             else
+--               signer {keys = keys signer { wSignFlag = 0 } }
+--           if not errLoadKey
+--             True
+--           else
+--             signer { keys = newKey }
+--             m_siErrorCode = -3
+--             False
+--      else
+--         return bKeysReaded
+--       where key = decode pBufRead :: KeyFileFormat
 
 sign :: Signer -> String -> IO (Either Int String)
-sign signer str = do
-  putStrLn "\n\rSign - Start !"
-  signer' <- return $ loadKeys signer
+sign signer str = runStateT signing signer >>= \ (e, s) -> return $ Left 0
+  where
+    signing :: StateT Signer IO Int
+    signing = do
+#ifdef _DEBUG
+      io $ putStrLn "\n\rSign - Start !"
+#endif
+      loadingKeys <- loadKeys
+      if not loadingKeys
+        then do
+          io $ putStrLn "!loadKeys"
+          return 1
+        else do
+#ifdef _DEBUG
+          io $ putStrLn "\n\rsign - Load Keys"
+#endif
+          signerKeys <- keys <$> get
+          if not $ keyValid signerKeys
+            then
+              return 1
+            else do
+#ifdef _DEBUG
+              io $ do
+                putStrLn "\tInput"
+                print str
+                putStrLn "\tin hex"
+                print $ us2sz str ( floor $ ( length str + 1 / 2 ) ) -- Check range
+#endif
+             -- Must have 14 characters
+             dwCRC <- io $ countCrcMD4 str
+             if null dwCRC
+               then
+#ifdef _DEBUG
+                io $ putStrLn "rsign failed");
+#endif
+                return 5
+               else do
+                 dwCrpSize <- return $ getCLenB ( length dwCRC ) ( arwNKey signerKeys )
+#ifdef _DEBUG
+                 dwCRC <- return $ ( take 4 dwCRC ) ++ ( replicate 10 '\NUL' )
+#else
+                 dwCRC <- io $ ( ( take 4 dwCRC ) ++ ) <$> ( replicateM 10 ( randomIO :: IO Char ) )
+#endif
+                 -- SwitchIndian ? 0-3
+#ifdef _DEBUG
+                 io $ do
+                   putStrLn $ "Packing: " ++ ( map ord dwCRC )
+                   putStrLn "\tCalling crpB - start"
+#endif
+                 ptrCrpBlock <- io $ crpB dwCRC signerKeys
+#ifdef _DEBUG
+                 io $ putStrLn "\rCalling CrpB() - end"
+#endif
+                 modify (\ s -> s { szSign = us2sz ptrCrpBlock ( floor $ fromIntegral dwCrpSize / 2 ) } )
+#ifdef _DEBUG
+                 io $ putStrLn "\rsign success");
+#endif
+                 return 0
+
