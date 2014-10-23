55bdbd3c9696c847cfc28138db1412b10a953163
Ilya Smelkov <triplepointfive@gmail.com>
2014-10-23 23:32:33 +0400

    Sign bytes

--- src/Signer.hs
+++ src/Signer.hs
@@ -2,7 +2,12 @@
 
 module Signer where
 
+import           Data.Bits (shiftR)
 import           Data.Word (Word8)
+import           Text.Printf (printf)
+
+import qualified Data.ByteString as B (ByteString, elem)
+import qualified Data.ByteString.Internal as B (c2w)
 
 import           Algebra (significance)
 
@@ -30,3 +35,25 @@ newSigner expon' modul = Signer { expon  = take exponentLength expon'
   where
     exponentLength = significance expon'
     modulusLength = significance modul
+
+sign :: Signer -> B.ByteString -> Bool -> IO String
+sign signer message randomEnabled =
+  if B.c2w '\r' `B.elem` message
+    then error "Message cannot contain of the following character: '\r'."
+    else do
+      signature <- signBytes signer message randomEnabled
+      return $ concat
+        [ if length signature > pos `div` ( intSize `div` shortSize )
+          then
+            let
+              shift = if 0 == pos `div` ( intSize `div` shortSize ) then 0 else shortSize :: Int
+              letter = signature !! pos `div` ( intSize `div` shortSize ) :: Int
+            in printf "%04x" $ letter `shiftR` shift
+          else
+            printf "%04x" ( 0 :: Int )
+          | pos <- [0, 1 .. keyLength signer `div` shortSize]
+        ]
+
+
+signBytes :: Signer -> B.ByteString -> Bool -> IO [Int]
+signBytes = undefined
