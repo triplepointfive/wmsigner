092a8de590bbb453452b42b38347506b1f45baed
Ilya Smelkov <triplepointfive@gmail.com>
2014-10-31 11:57:00 +0300

    Get bits count

--- src/Algebra.hs
+++ src/Algebra.hs
@@ -1,5 +1,6 @@
 module Algebra where
 
+import Data.Bits (testBit, bitSize, Bits)
 import Data.Int (Int32)
 
 significance :: [a] -> Int
@@ -7,3 +8,11 @@ significance = undefined
 
 remainder :: [Int32] -> [Int32] -> [Int32]
 remainder lhs rhs = undefined
+
+getBitsCount :: Bits a => [a] -> Int
+getBitsCount xs = ( vLenght -1 ) * 32 + ( 32 - ( numberOfLeadingZeros $ last xs ) )
+  where vLenght = length xs -- significand?
+
+numberOfLeadingZeros :: Bits a => a -> Int
+numberOfLeadingZeros x = length $ takeWhile (not . testBit x) [ size -1 , size - 2 .. 0]
+  where size = bitSize x
