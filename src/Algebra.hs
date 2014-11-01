e8de29a7266ee8799e744e5f2d0ae257856f5018
Ilya Smelkov <triplepointfive@gmail.com>
2014-11-01 13:29:35 +0300

    Lists comparing and base remainder

--- src/Algebra.hs
+++ src/Algebra.hs
@@ -1,18 +1,70 @@
 module Algebra where
 
-import Data.Bits (testBit, bitSize, Bits)
+import Data.Bits (testBit, bitSize, Bits, (.&.))
 import Data.Int (Int32)
 
+longMask :: Int32
+longMask = 0xFFFFFFFF
+
+compareLists :: [Int32] -> [Int32] -> Ordering
+compareLists lhs rhs
+    | lhsLenght > rhsLenght = GT
+    | lhsLenght < rhsLenght = LT
+    | otherwise             = comp (reverse lhs) (reverse rhs)
+  where
+    lhsLenght = significance lhs
+    rhsLenght = significance rhs
+
+    comp [] _ = EQ
+    comp _ [] = EQ
+    comp (l:ls) (r:rs)
+      | lb > rb   = GT
+      | lb < rb   = LT
+      | otherwise = comp ls rs
+      where
+        lb = l .&. longMask
+        rb = r .&. longMask
+
 significance :: [a] -> Int
-significance = undefined
+significance = length
+
+shift :: [a] -> Int -> [a]
+shift = undefined
+
+shiftRight :: [a] -> [a]
+shiftRight = undefined
+
+sub :: [a] -> [a] -> [a]
+sub = undefined
 
 remainder :: [Int32] -> [Int32] -> [Int32]
-remainder lhs rhs = undefined
+remainder lhs rhs = divide lhs rhs
+  where
+    rhsBitsCount = getBitsCount rhs -- check attemption to divide by zero
+
+    divide :: [Int32] -> [Int32] -> [Int32]
+    divide l r
+      | LT /= compareLists l r = l
+      | lhsBitsCount == 0      = l
+      | otherwise              =
+        let temp' = if compareLists l temp == LT then shiftRight temp else temp
+        in divide ( subs l temp' ) r
+      where
+        lhsBitsCount = getBitsCount l
+        temp         = shift r (lhsBitsCount - rhsBitsCount)
+    subs :: [Int32] -> [Int32] -> [Int32]
+    subs l t =
+      if compareLists l t /= LT
+        then subs (sub l t) t
+        else l
 
 getBitsCount :: Bits a => [a] -> Int
-getBitsCount xs = ( vLenght -1 ) * 32 + ( 32 - ( numberOfLeadingZeros $ last xs ) )
-  where vLenght = length xs -- significand?
+getBitsCount xs = ( vLenght -1 ) * 32 + ( getBitsNumber ( last xs ) )
+  where vLenght = significance xs
+
+getBitsNumber :: Bits a => a -> Int
+getBitsNumber x = 32 - numberOfLeadingZeros x
 
 numberOfLeadingZeros :: Bits a => a -> Int
-numberOfLeadingZeros x = length $ takeWhile (not . testBit x) [ size -1 , size - 2 .. 0]
+numberOfLeadingZeros x = length $ takeWhile (not . testBit x) [size - 1, size - 2 .. 0]
   where size = bitSize x
