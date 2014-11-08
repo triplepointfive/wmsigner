6789a6d8dd0d7ba1e93ca7c5c72abd7bfa0b493a
Ilya Smelkov <triplepointfive@gmail.com>
2014-11-08 12:29:12 +0300

    Sign bytes

--- src/Signer.hs
+++ src/Signer.hs
@@ -3,16 +3,18 @@
 module Signer where
 
 import           Data.Bits (shiftR, shiftL, (.|.))
+import           Data.Int (Int8, Int32)
 import           Data.List (foldl')
 import           Data.Word (Word8, Word32)
 import           Text.Printf (printf)
 
 import           Crypto.Hash.MD4 (hash)
-import qualified Data.ByteString as B (ByteString, elem, unpack)
+import qualified Data.ByteString as B (ByteString, elem, unpack, pack)
 import qualified Data.ByteString.Internal as B (c2w)
 import           Data.List.Split (chunksOf)
 
 import           Algebra (significance)
+import           Montgomery (exponentation)
 
 intSize, shortSize, byteSize, hashSize :: Int
 intSize   = 32
@@ -25,15 +27,17 @@ header = [0x38, 0]
 trail = [1, 0]
 
 data Signer where
-  Signer :: { expon     :: ![Word32]
-            , modulus   :: ![Word32]
+  Signer :: { expon     :: ![Int32]
+            , modulus   :: ![Int32]
             , keyLength :: !Int
             } -> Signer
 
-newSigner :: [Word8] -> [Word8] -> Signer
+instance Show Signer
+
+newSigner :: [Int8] -> [Int8] -> Signer
 newSigner expon' modul = Signer { expon     = cast expon'
                                 , modulus   = modulus'
-                                , keyLength = (length modulus') * byteSize
+                                , keyLength = (significance modul) * byteSize
                                 }
   where modulus' = cast modul
 
@@ -55,8 +59,9 @@ newSigner expon' modul = Signer { expon     = cast expon'
 --           | pos <- [0, 1 .. keyLength signer `div` shortSize]
 --         ]
 
--- signBytes :: Signer -> B.ByteString -> Bool -> IO [Int]
-signBytes signer message randomEnabled = cast blob
+signBytes :: Signer -> B.ByteString -> Bool -> [Int32]
+signBytes signer message randomEnabled = error $ show [(cast blob),(expon signer),(modulus signer)]
+    -- exponentation (cast blob) (expon signer) (modulus signer)
   where
     blob = header
            ++ B.unpack ( hash message )
@@ -66,12 +71,11 @@ signBytes signer message randomEnabled = cast blob
                       - ( length header )
                       - hashSize `div` byteSize
                       - ( length trail )
---     bytes = B.unpack message
 
-cast :: [Word8] -> [Word32]
+cast :: Integral a => [a] -> [Int32]
 cast xs = map ( fromOctets . reverse ) $ chunksOf 4 xs
 
-fromOctets :: [Word8] -> Word32
+fromOctets :: Integral a => [a] -> Int32
 fromOctets = foldl' accum 0
   where
     accum a o = (a `shiftL` 8) .|. fromIntegral o
