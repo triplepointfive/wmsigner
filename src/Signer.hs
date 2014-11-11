31c31f4aaf3d2a2a33106efa00c17ac4ee14aa5a
Ilya Smelkov <triplepointfive@gmail.com>
2014-11-11 14:57:05 +0300

    Complete base sign

--- src/Signer.hs
+++ src/Signer.hs
@@ -3,7 +3,7 @@
 module Signer where
 
 import           Data.Bits (shiftR, shiftL, (.|.))
-import           Data.Int (Int8, Int32)
+import           Data.Int (Int8, Int16, Int32)
 import           Data.List (foldl')
 import           Data.Word (Word8, Word32)
 import           Text.Printf (printf)
@@ -13,7 +13,7 @@ import qualified Data.ByteString as B (ByteString, elem, unpack, pack)
 import qualified Data.ByteString.Internal as B (c2w)
 import           Data.List.Split (chunksOf)
 
-import           Algebra (significance)
+import           Algebra (significance, logicalShiftRight)
 import           Montgomery (exponentation)
 
 intSize, shortSize, byteSize, hashSize :: Int
@@ -39,23 +39,25 @@ newSigner expon' modul = Signer { expon     = cast expon'
                                 }
   where modulus' = cast modul
 
--- sign :: Signer -> B.ByteString -> Bool -> IO String
--- sign signer message randomEnabled =
---   if B.c2w '\r' `B.elem` message
---     then error "Message cannot contain of the following character: '\r'."
---     else do
---       signature <- signBytes signer message randomEnabled
---       return $ concat
---         [ if length signature > pos `div` ( intSize `div` shortSize )
---           then
---             let
---               shift = if 0 == pos `div` ( intSize `div` shortSize ) then 0 else shortSize :: Int
---               letter = signature !! pos `div` ( intSize `div` shortSize ) :: Int
---             in printf "%04x" $ letter `shiftR` shift
---           else
---             printf "%04x" ( 0 :: Int )
---           | pos <- [0, 1 .. keyLength signer `div` shortSize]
---         ]
+sign :: Signer -> B.ByteString -> Bool -> String
+sign signer message randomEnabled =
+  if B.c2w '\r' `B.elem` message
+    then error "Message cannot contain of the following character: '\r'."
+    else buildSignature signer signature
+  where signature = signBytes signer message randomEnabled
+
+buildSignature :: Signer -> [Int32] -> String
+buildSignature signer signature = concat --error $ show
+    [ if length signature > pos `div` ( intSize `div` shortSize )
+      then
+        let
+          shift = if 0 == pos `mod` ( intSize `div` shortSize ) then 0 else shortSize :: Int
+          letter = signature !! (fromIntegral $ pos `div` ( intSize `div` shortSize ))
+        in printf "%04x" $ (fromIntegral (letter `logicalShiftRight` shift) :: Int16)
+      else
+        printf "%04x" ( 0 :: Int )
+      | pos <- [0, 1 .. keyLength signer `div` shortSize - 1]
+    ]
 
 signBytes :: Signer -> B.ByteString -> Bool -> [Int32]
 signBytes signer message randomEnabled = exponentation (cast blob) (expon signer) (modulus signer)
