4f5f0adaad3ae4ba3793cd9dcf55165de6332b96
Ilya Smelkov <triplepointfive@gmail.com>
2014-11-26 19:44:23 +0300

    Complete docs

--- src/Crypto/Webmoney.hs
+++ src/Crypto/Webmoney.hs
@@ -1,8 +1,20 @@
 module Crypto.Webmoney
     (
-      module Crypto.Internal.Signer
+      newSigner
+    , sign
+    , signUnsafe
+    , S.Signer
     )
     where
 
 import qualified Crypto.Internal.Signer as S (Signer, newSigner, sign,
                                               signUnsafe)
+
+-- |Initializes new 'Signer' object, takes exponent and modulus as arguments
+newSigner = S.newSigner
+
+-- |Calculates static signature for string
+signUnsafe = S.signUnsafe
+
+-- |Calculates randomized signature for string
+sign = S.sign
